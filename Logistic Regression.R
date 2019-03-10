917fe8d6a1aebc019044a43506a15d356d6561aa
Outliers001 <48390230+Outliers001@users.noreply.github.com>
2019-03-10 17:19:02 +0600

    Add files via upload

---
+++ Logistic Regression.R
@@ -0,0 +1,79 @@
+url <- "http://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+data <- read.csv(url, header=FALSE)
+
+colnames(data) <- c(
+  "age",
+  "sex",# 0 = female, 1 = male
+  "cp", # chest pain
+  # 1 = typical angina,
+  # 2 = atypical angina,
+  # 3 = non-anginal pain,
+  # 4 = asymptomatic
+  "trestbps", # resting blood pressure (in mm Hg)
+  "chol", # serum cholestoral in mg/dl
+  "fbs",  # fasting blood sugar if less than 120 mg/dl, 1 = TRUE, 0 = FALSE
+  "restecg", # resting electrocardiographic results
+  # 1 = normal
+  # 2 = having ST-T wave abnormality
+  # 3 = showing probable or definite left ventricular hypertrophy
+  "thalach", # maximum heart rate achieved
+  "exang",   # exercise induced angina, 1 = yes, 0 = no
+  "oldpeak", # ST depression induced by exercise relative to rest
+  "slope", # the slope of the peak exercise ST segment
+  # 1 = upsloping
+  # 2 = flat
+  # 3 = downsloping
+  "ca", # number of major vessels (0-3) colored by fluoroscopy
+  "thal", # this is short of thalium heart scan
+  # 3 = normal (no cold spots)
+  # 6 = fixed defect (cold spots during rest and exercise)
+  # 7 = reversible defect (when cold spots only appear during exercise)
+  "hd" # (the predicted attribute) - diagnosis of heart disease
+  # 0 if less than or equal to 50% diameter narrowing
+  # 1 if greater than 50% diameter narrowing
+)
+data$sex[data$sex==0]="F"
+data$sex[data$sex==1]="M"
+data$sex=as.factor(data$sex)
+data$cp <- as.factor(data$cp)
+data$fbs <- as.factor(data$fbs)
+data$restecg <- as.factor(data$restecg)
+data$exang <- as.factor(data$exang)
+data$slope <- as.factor(data$slope)
+data[data == "?"] <- NA
+data$ca <- as.integer(data$ca) # since this column had "?"s in it
+# R thinks that the levels for the factor are strings, but
+# we know they are integers, so first convert the strings to integiers...
+data$ca <- as.factor(data$ca)  # ...then convert the integers to factor levels
+
+data$thal <- as.integer(data$thal) # "thal" also had "?"s in it.
+data$thal <- as.factor(data$thal)
+
+## This next line replaces 0 and 1 with "Healthy" and "Unhealthy"
+data$hd <- ifelse(test=data$hd == 0, yes="Healthy", no="Unhealthy")
+data$hd <- as.factor(data$hd)
+
+
+nrow(data[is.na(data$ca) | is.na(data$thal),])
+data[is.na(data$ca) | is.na(data$thal),]
+nrow(data)
+data <- data[!(is.na(data$ca) | is.na(data$thal)),]
+nrow(data)
+
+xtabs(~hd+sex, data=data)
+xtabs(~ hd + cp, data=data)
+xtabs(~ hd + fbs, data=data)
+xtabs(~ hd + restecg, data=data)
+xtabs(~ hd + exang, data=data)
+xtabs(~ hd + slope, data=data)
+xtabs(~ hd + ca, data=data)
+xtabs(~ hd + thal, data=data)
+
+logistic <- glm(hd ~ sex, data=data, family="binomial")
+summary(logistic)
+
+female.log.odds <- log(25 / 71)
+female.log.odds
+
+
+predicted.data <- data.frame(probability.of.hd=logistic$fitted.values,sex=data$sex)
